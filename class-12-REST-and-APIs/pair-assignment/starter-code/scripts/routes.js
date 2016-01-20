f1fb41911fc0b21a84355a63abe6675bca224f78
Brandon Feinstein <brandon.feinstein@Hotmail.com>
2016-01-19 16:11:10 -0800

    fixed page base

--- class-12-REST-and-APIs/pair-assignment/starter-code/scripts/routes.js
+++ class-12-REST-and-APIs/pair-assignment/starter-code/scripts/routes.js
@@ -1,5 +1,8 @@
 // DONE: Configure routes for this app with page.js, by registering each URL your app can handle,
 // linked to a a single controller function to handle it:
+
+page.base('');
+
 page('/', articlesController.index);
 page('/about', aboutController.index);
 
